dfbbec223cc1bd3ba66154c117521b4b9eb8695d
Eriquy Jhordan <eriquy@gmail.com>
2022-05-10 19:52:40 -0300

    feat: create dark and light theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,18 @@
+export default {
+  title: 'dark',
+  brandColors: {
+    brand: '#8257E5',
+    brandHover: '#996DFF',
+    textOnBrand: '#FFFFFF',
+  },
+  colors: {
+    surfacePrimary: '#18181b',
+    surfaceSecondary: '#27272a',
+    surfaceSecondaryHover: '#3f3f46',
+    stroke: '#52525b',
+    tooltip: '#f4f4f5',
+    textPrimary: '#f4f4f5',
+    textSecondary: '#a1a1aa',
+    textOnTooltip: '#27272a',
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+export default {
+  title: 'light',
+  brandColors: {
+    brand: '#8257E5',
+    brandHover: '#996DFF',
+    textOnBrand: '#FFFFFF',
+  },
+  colors: {
+    surfacePrimary: '#FFFFFF',
+    surfaceSecondary: '#f4f4f5',
+    surfaceSecondaryHover: '#e4e4e7',
+    stroke: '#d4d4d8',
+    tooltip: '#27272a',
+    textPrimary: '#27272a',
+    textSecondary: '#71717a',
+    textOnTooltip: '#f4f4f5',
+  },
+};
