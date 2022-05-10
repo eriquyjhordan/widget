0131788356bce605086ff9b1a77a87e254e9a7c6
Eriquy Jhordan <eriquy@gmail.com>
2022-05-10 19:52:56 -0300

    feat: apply dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/global.style';
+// import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Start application</h1>
-    </div>
+    <ThemeProvider theme={dark}>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Start application</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
